efa777c54a338e2d8392d65ef3a77c44afa36d72
Łukasz OSypiuk <osypiuklukasz@gmail.com>
2022-06-19 17:36:49 +0200

    Added grid vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GridLayout />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GridLayout from './components/GridLayout.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GridLayout
   }
 }
 </script>
